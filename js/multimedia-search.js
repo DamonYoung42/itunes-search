03a9c5e6ea3320e5e53768e7e799b292e8f44761
DamonYoung42 <damonyoung@wi.rr.com>
2016-09-30 18:04:46 -0500

    formatting layout.

--- js/multimedia-search.js
+++ js/multimedia-search.js
@@ -1,142 +1,169 @@
-
-
 function executeSearch(){
-	try{
+	$(document).ready(function(){
+		try{
 
-		if ($('#searchTerm').val() === ""){
-			alert("Please enter a search term.");
-		}
-		else{
-			searchiTunes();
-			searchYouTube();	
-		}
-	}
-	catch(err){
-		console.log(err);
-	}
+			if ($('#searchTerm').val() === ""){
+				alert("Please enter a search term.");
+			}
+			else{
+					
+				if ($('input[name=searchService]:checked').val() === "Both"){
+					searchiTunes();
+					searchYouTube();			
+				}
+				else if	($('input[name=searchService]:checked').val() === "iTunes"){
+					hideYouTube();
+					searchiTunes();
+				}
+				else{
+					hideiTunes();
+					searchYouTube();
+				}
 
+			}
+		}
+		catch(err){
+			console.log(err);
+		}
+	});
 }
 
 
 function searchiTunes(){
-	try{
-		$.getJSON('https://itunes.apple.com/search?term='+formatCriteria($('#searchTerm').val())+'&media=music&limit=100&callback=?', 
-		  	function(items) {
-				var songs = "";
+	$(document).ready(function(){
 
-				$.each(items.results, function(index,song){
-					var displayItem = '<div class="row">';
-					displayItem += '<div class="col-sm-3"><center>';
-					displayItem += '<img class="img-thumbnail" src="' + song.artworkUrl100 + '">';
-					displayItem += '</center></div>';
-					displayItem += '<div class="col-sm-3"><center>';
-					displayItem += '<h5>' + song.trackName + '</h5>';
-					displayItem += '<h5>'+ song.artistName +'</h5>';
-					displayItem += '<p>'+ song.collectionName;
-					displayItem += '</center></div>';
-					displayItem += '<div class="col-sm-3"><center>';			
-					displayItem += '<p>Collection Price: ' + song.collectionPrice;
-					displayItem += '<br>Track Price: ' + song.trackPrice + '</p></center></div>';
-					displayItem += '<div class="col-sm-3"><center>';
-					displayItem += '<audio controls><source src="'+ song.previewUrl + '" type="audio/mpeg">';
-					displayItem += 'Your browser does not support the audio element.</audio>';
-					displayItem += '</center></div>'
-					displayItem += '<div class="col-sm-12"><hr></div></div>';
-					songs += displayItem;
+		try{
+			$.getJSON('https://itunes.apple.com/search?term='+formatCriteria($('#searchTerm').val())+'&media=music&limit=100&callback=?', 
+			  	function(data) {
+					var songs = "";
 
-				});
-/*				$.each(items.results, function(index, song){
+					$.each(data.results, function(index,song){
+						var displayItem = '<div class="row">';
+						displayItem += '<div class="col-sm-3"><center>';
+						displayItem += '<img class="img-thumbnail" src="' + song.artworkUrl100 + '">';
+						displayItem += '</center></div>';
+						displayItem += '<div class="col-sm-2"><center>';
+						displayItem += '<h5>' + song.trackName + '</h5>';
+						displayItem += '<h5>'+ song.artistName +'</h5>';
+						displayItem += '<p>'+ song.collectionName;
+						displayItem += '</center></div>';
+						displayItem += '<div class="col-sm-2"><center>';			
+						displayItem += '<p>Collection Price: ' + song.collectionPrice;
+						displayItem += '<br>Track Price: ' + song.trackPrice + '</p></center></div>';
+						displayItem += '<div class="col-sm-5"><center>';
+						displayItem += '<audio controls><source src="'+ song.previewUrl + '" type="audio/mpeg">';
+						displayItem += 'Your browser does not support the audio element.</audio>';
+						displayItem += '</center></div>'
+						displayItem += '<div class="col-sm-12"><hr></div></div>';
+						songs += displayItem;
 
-					var displayItem = '<div class="col-sm-6"><center>'
-					displayItem += '<img class="img-thumbnail" src="' + song.artworkUrl100 + '">';
-					displayItem += '<h5>' + song.trackName + '</h5>';
-					displayItem += '<h5>'+ song.artistName +'</h5>';
-					displayItem += '<p class="text-muted">'+ song.collectionName;
-					displayItem += '<br>Collection Price: ' + song.collectionPrice;
-					displayItem += '<br>Track Price: ' + song.trackPrice + '</p>';
-					displayItem += '<audio controls><source src="'+ song.previewUrl +'" type="audio/mpeg">';
-					displayItem += 'Your browser does not support the audio element.</audio>';
-					displayItem += '</center></div>';
-					songs += displayItem;	
-				});*/
-
-	            if(items.resultCount === 0)
-	            {
-	            	$('#iTunesHeadline').hide();
-					var noResultsMessage = "No items found in the iTunes library for " + $('#searchTerm').val();
-					$('#results').html('<div class="col-md-12"><center><h4>' + noResultsMessage + '</h4></center></div>');
-					$('#iTunesPagination').hide();
-	            }
-	            else{
-	            	$('#iTunesHeadline').show();
-	            	$('#results').html(songs);
-
-		            //jPages
-		            $(function() {
-						$("#iTunesPagination").jPages({
-							containerID: "results"
-						});
 					});
-					$('iTunesPagination').show();
-	            }
-        	});
-	}
-	catch (err){
-		console.log(err);
-	}
+
+		            if(data.resultCount === 0)
+		            {
+		            	$('#iTunesHeadline').hide();
+		            	$('#iTunesPagination').hide();
+		            	$('#iTunesResults').show();
+						var noResultsMessage = "No items found in the iTunes library for " + $('#searchTerm').val();
+						$('#iTunesResults').html('<div class="col-md-12"><center><h4>' + noResultsMessage + '</h4></center></div>');
+
+		            }
+		            else{
+						showiTunes();
+		            	$('#iTunesResults').html(songs);
+
+			            //jPages
+			            $(function() {
+							$("#iTunesPagination").jPages({
+								containerID: "iTunesResults"
+							});
+						});				
+		            }
+	        	});
+		}
+		catch (err){
+			console.log(err);
+		}
+	});
 }
 
 function searchYouTube(){
-	try{
-		$.ajax({
-			url: 'https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&maxResults=25&order=relevance&q='+formatCriteria($('#searchTerm').val())+'&key=AIzaSyC9IcvsoIFHxGEkRcgGZWx48gDKmTe7muQ',
-			dataType: 'json',
-			type: 'GET',
-			success: function(data) {
-				var videos = '';
-				$.each(data.items, function(index, video){
-				var displayItem = '<div class="row">';
-				displayItem += '<div class="col-sm-6"><center>';
-				displayItem += '<img class="img-thumbnail" src="' + video.snippet.thumbnails.default.url + '">';
-				//displayItem += '<iframe width="300" height="120" src="https://www.youtube.com/embed/' + video.id.videoId + '"></iframe>';
-				displayItem += '</center></div>';
-				displayItem += '<div class="col-sm-6"><center>';
-				displayItem += '<h5><a href="https://www.youtube.com/watch?v=' + video.id.videoId + '" target="_blank">Watch: ' + video.snippet.title +'</a></h5>';
-				displayItem += '</center></div>';
-				displayItem += '<div class="col-sm-12"><hr></div></div>';
-				videos += displayItem;
-				});
-
-				if(data.items.length === 0){
-					$('#YouTubeHeadline').hide();
-					$('#YouTubePagination').hide();
-
-					var noResultsMessage = "No items found in the YouTube library for " + $('#searchTerm').val();
-
-					$('#videoResults').html('<div class="col-md-12"><center><h4>' + noResultsMessage + '</h4></center></div>');
-				}
-				else{
-					$('#YouTubeHeadline').show();
-					$('#videoResults').html(videos);
-
-					//jPages
-					$(function() {
-						$("#YouTubePagination").jPages({
-							containerID: "videoResults"
-						});
+	$(document).ready(function(){
+		try{
+			$.ajax({
+				url: 'https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&maxResults=25&order=relevance&q='+formatCriteria($('#searchTerm').val())+'&key=AIzaSyC9IcvsoIFHxGEkRcgGZWx48gDKmTe7muQ',
+				dataType: 'json',
+				type: 'GET',
+				success: function(data) {
+					var videos = '';
+					$.each(data.items, function(index, video){
+					var displayItem = '<div class="row">';
+					displayItem += '<div class="col-sm-6"><center>';
+					displayItem += '<img class="img-thumbnail" src="' + video.snippet.thumbnails.default.url + '">';
+					//displayItem += '<iframe width="300" height="120" src="https://www.youtube.com/embed/' + video.id.videoId + '"></iframe>';
+					displayItem += '</center></div>';
+					displayItem += '<div class="col-sm-6"><center>';
+					displayItem += '<h5><a href="https://www.youtube.com/watch?v=' + video.id.videoId + '" target="_blank">Watch: ' + video.snippet.title +'</a></h5>';
+					displayItem += '</center></div>';
+					displayItem += '<div class="col-sm-12"><hr></div></div>';
+					videos += displayItem;
 					});
 
-					$('#YouTubePagination').show();
+					if(data.items.length === 0){
+						$('#YouTubeHeadline').hide();
+						$('#YouTubePagination').hide();
+						$('#YouTubeResults').show();
+
+						var noResultsMessage = "No items found in the YouTube library for " + $('#searchTerm').val();
+
+						$('#YouTubeResults').html('<div class="col-md-12"><center><h4>' + noResultsMessage + '</h4></center></div>');
+					}
+					else{
+						showYouTube();
+						$('#YouTubeResults').html(videos);
+
+						//jPages
+						$(function() {
+							$("#YouTubePagination").jPages({
+								containerID: "YouTubeResults"
+							});
+						});
+
+
+					}
 				}
-			}
-		});
-	}
-	catch(err){
-		console.log(err);
-	}
+			});
+			
+		}
+		catch(err){
+			console.log(err);
+		}
+	});
 }
 
+function hideYouTube(){
+	$('#YouTubeHeadline').hide();
+	$('#YouTubeResults').hide();
+	$('#YouTubePagination').hide();
+}
 
+function hideiTunes(){
+	$('#iTunesHeadline').hide();
+	$('#iTunesResults').hide();
+	$('#iTunesPagination').hide();
+}
+
+function showYouTube(){
+	$('#YouTubeHeadline').show();
+	$('#YouTubeResults').show();
+	$('#YouTubePagination').show();
+}
+
+function showiTunes(){
+	$('#iTunesHeadline').show();
+	$('#iTunesResults').show();
+	$('#iTunesPagination').show();
+}
 function formatCriteria(criteria){
 	return criteria.trim().replace(/ /g, "+");
 }
